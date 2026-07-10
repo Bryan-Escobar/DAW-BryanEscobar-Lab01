374ceaabe26c005ab76c3babd742b15ffebac3f1
Bryan-Escobar <bryan.escobar2@catolica.edu.sv>
2026-07-09 22:31:01 -0600

    commmit inicial

---
+++ BryanEscobar.Lab01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BryanEscobar.Lab01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
