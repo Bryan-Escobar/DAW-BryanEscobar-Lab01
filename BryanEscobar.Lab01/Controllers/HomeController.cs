74b406cfc6ea2520f0d1a85d4885d4a0e391b124
Bryan-Escobar <bryan.escobar2@catolica.edu.sv>
2026-07-09 23:05:24 -0600

    Se personalizó   Views/Home/Index.cshtml con la información del estudiante (nombre, carrera y asignatura) cargada desde ViewBag en   HomeController. Se agregó la sección Sobre mí, la foto de perfil en wwwroot/images, la fecha y hora dinámica con   @DateTime.Now, y el roadmap profesional con metas, progress bar, badges, list group y botón Bootstrap. Se configuró   Bootstrap Icons en el layout y se añadió .gitignore para excluir carpetas de compilación y archivos de Visual Studio

--- BryanEscobar.Lab01/Controllers/HomeController.cs
+++ BryanEscobar.Lab01/Controllers/HomeController.cs
@@ -8,11 +8,20 @@ namespace BryanEscobar.Lab01.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
+            ViewBag.Nombre = "Bryan Steven Escobar Preza";
+            ViewBag.Carrera = "Ingeniería en Desarrollo de Software";
+            ViewBag.Asignatura = "Desarrollo Multiplataforma de Aplicaciones Web";
+            ViewBag.SobreMi = "Desarrollador Full Stack especializado en React Native, Node.js y Agentes de IA. Combino mi base técnica con una profunda pasión por la comunicación y socialización de ideas. Creo firmemente que los mejores productos tecnológicos no solo requieren un código impecable, sino la habilidad de saber expresarlos y conectar con las personas.\r\n\r\n";
+            ViewBag.MetasProfesionales = new List<string>
+            {
+                "Graduarme de ingeniería mientras acumulo experiencia inicial.",
+                "Convertirme en líder de equipo.",
+                "Ser líder de proyecto.",
+                "Escalar y poder presentar productos de software en grandes conferencias con pitches profesionales.",
+                "Convertirme en CTO de alguna compañía y desarrollar una habilidad en comunicación corporativa con excelencia."
+            };
+            ViewBag.EtapaActual = 1;
 
-        public IActionResult Privacy()
-        {
             return View();
         }
 
--- BryanEscobar.Lab01/obj/Debug/net10.0/BryanEscobar.Lab01.AssemblyInfo.cs
+++ BryanEscobar.Lab01/obj/Debug/net10.0/BryanEscobar.Lab01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BryanEscobar.Lab01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+374ceaabe26c005ab76c3babd742b15ffebac3f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BryanEscobar.Lab01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BryanEscobar.Lab01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
